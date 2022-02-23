593d2fd8082b7240b141b48fc5da5c608d71f177
kjeg2 <kjeg2@cam.ac.uk>
2022-02-23 09:15:36 +0000

    checkerboard

--- camera_test.py
+++ camera_test.py
@@ -1,12 +1,6 @@
 import cv2
 
-<<<<<<< HEAD
 r = requests.get(url= 'http://localhost:8081/stream/video.mjpeg', stream=True)
-=======
-#print("Before URL")
-cap = cv2.VideoCapture('rtsp://admin:123456@192.168.1.216/H264?ch=1&subtype=0')
-#print("After URL")
->>>>>>> be3d6ea475683bc6bde35bb665b77148fb4a71c5
 
 while True:
 
